7fd8fc5e7a38e2868b8ee2e490254b139f17e417
Joshua McCue <email>
2020-09-18 16:43:19 -0400

    Updates

---
+++ precache-manifest.5b040928dd7e99f6224703889cff764b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7eeb9a1d51140deabc7669560c9271e",
+    "url": "/catan-resource-distributor/index.html"
+  },
+  {
+    "revision": "77cca536959ec0d79b46",
+    "url": "/catan-resource-distributor/static/css/main.7ff9c2f8.chunk.css"
+  },
+  {
+    "revision": "ec0101d7e014bbaac0e6",
+    "url": "/catan-resource-distributor/static/js/2.88ef4aa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catan-resource-distributor/static/js/2.88ef4aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77cca536959ec0d79b46",
+    "url": "/catan-resource-distributor/static/js/main.895a9c19.chunk.js"
+  },
+  {
+    "revision": "8615c835663d2a52b26e",
+    "url": "/catan-resource-distributor/static/js/runtime-main.9df9b949.js"
+  },
+  {
+    "revision": "7b82549f12cb054bfb8baf0a98363ab1",
+    "url": "/catan-resource-distributor/static/media/brick.7b82549f.jpg"
+  },
+  {
+    "revision": "038a5934e0afa349d92d2d19ac91017e",
+    "url": "/catan-resource-distributor/static/media/lumber.038a5934.png"
+  },
+  {
+    "revision": "44f6de88390838800936b62aad247887",
+    "url": "/catan-resource-distributor/static/media/robber.44f6de88.png"
+  },
+  {
+    "revision": "26ee0bf645ba9dd77711b9b8b0e5cc15",
+    "url": "/catan-resource-distributor/static/media/wheat.26ee0bf6.jpg"
+  },
+  {
+    "revision": "c4a3729bce43f649a8aca4ee883345de",
+    "url": "/catan-resource-distributor/static/media/wool.c4a3729b.jpg"
+  }
+]);
